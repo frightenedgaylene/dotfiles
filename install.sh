1647a9c6f261a2ba8e5810cd9af3c24586694bf7
frightenedgaylene <frightenedgaylene@proton.me>
2025-06-26 16:42:10 +0300

    add fish

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+fish_config="$HOME/.config/fish/config.fish"
 
+if [[ -e "$fish_config" && -L "$fish_config" ]]; then
+	echo fish config already installed
+else
+	ln -s $(pwd)/config.fish $fish_config
+	echo fish config done
+fi
